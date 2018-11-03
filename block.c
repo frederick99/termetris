a250a168ec64aaec4d4318aab529d36e8fa2d9f3
Naveen <fredericktrotteville99@gmail.com>
2018-11-03 19:33:11 +0530

    added code for blocks

---
+++ block.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int kinds[][4][2] = {
+  {{-1,0}, {0,0}, {1,0}, {2,0}},  // oooo
+  {{-1,0}, {0,0}, {1,0}, {0,1}},  // o8o
+  {{0,0}, {1,0}, {0,1}, {1,1}},   // 88
+  {{0,0}, {0,1}, {1,0}, {2,0}},   // 8oo
+  {{-2,0}, {-1,0}, {0,0}, {0,1}}, // oo8
+  {{-1,0}, {0,0}, {0,1}, {1,1}},  // o8º
+  {{-1,1}, {0,1}, {0,0}, {1,0}},  // º8o
+};
+
+
+struct Block {
+  int type;
+  int x,y;
+  int orientation;
+  int cells[4][2];
+};
+
+void display_coods(struct Block* block){
+  for(int i=0;i<4;i++){
+    printf("%d,%d\n",block->cells[i][0],block->cells[i][1]);
+    }
+    printf("-----\n");
+}
+
+void getcood(struct Block* block){
+  if(block->orientation==1){
+    for(int i=0;i<4;i++){
+      int temp  = block->cells[i][0];
+      block->cells[i][0] = block->cells[i][1];
+      block->cells[i][1] = -1*temp;
+    }
+  }
+  else if(block->orientation==3){
+    for(int i=0;i<4;i++){
+      int temp  = block->cells[i][0];
+      block->cells[i][0] = -1*block->cells[i][1];
+      block->cells[i][1] = temp;
+    }
+  }
+  if(block->orientation==2){
+    for(int i=0;i<4;i++){
+      block->cells[i][0] = -1*block->cells[i][0];
+      block->cells[i][1] *=-1;
+    }
+  }
+}
+
+struct Block get_block() {
+  int k = rand() % 7;
+  struct Block block = {0};
+  block.type = k;
+  //block.cells = kinds[i];
+  for(int i=0;i<4;i++){
+    block.cells[i][0] = kinds[k][i][0];
+    block.cells[i][1] = kinds[k][i][1];
+  }
+  block.orientation = rand() % 4;
+  display_coods(&block);
+  getcood(&block);
+  display_coods(&block);
+  return block;
+}
+
+
+
+int min_x_cood(struct Block* block){
+  int temp  = 1000;
+    for(int i=0;i<4;i++){
+      if(temp>block->cells[i][0])
+        temp = block->cells[i][0];
+    }
+    return temp;
+}
+
+int max_x_cood(struct Block* block){
+  int temp  = -1000;
+    for(int i=0;i<4;i++){
+      if(temp<block->cells[i][0])
+        temp = block->cells[i][0];
+    }
+    return temp;
+}
+
+void Dmain() {
+  srand(time(0));
+  struct Block b = get_block();
+  printf("%d %d\n", b.type, b.orientation);
+  for (int i = 0; i < 4; i++)
+    printf("%d %d\n", b.cells[i][0], b.cells[i][1]);
+};
---
+++ main.c
@@ -0,0 +1,47 @@
+#include <ncurses.h>
+#include <time.h>
+#include "block.c"
+
+#define WIDTH 16
+
+void rotateblock(struct Block* block,int direction){
+  if(direction==1){
+    for(int i=0;i<4;i++){
+      int temp  = block->cells[i][0];
+      block->cells[i][0] = block->cells[i][1];
+      block->cells[i][1] = -1*temp;
+    }
+  }
+  else if(direction==-1){
+    for(int i=0;i<4;i++){
+      int temp  = block->cells[i][0];
+      block->cells[i][0] = -1*block->cells[i][1];
+      block->cells[i][1] = temp;
+    }
+  }
+}
+
+void insert_in_grind(grid)
+
+int main(int argc, char const *argv[])
+{
+  // initscr();
+  // printw("Hello, world!");
+  // refresh();
+  // getch();
+  // endwin();
+  int grid[20][16] ={0} ;
+  int count = 1;
+  while(count){
+    struct Block cur_block = get_block();
+    display_coods(&cur_block);
+    int starting_x = rand()%(WIDTH - max_x_cood(&cur_block)) + -1*min_x_cood(&cur_block) ;
+    cur_block.x = starting_x;
+    count--;
+
+
+  }
+
+
+  return 0;
+}
